fbe0a88a31ed3034789bfabed65f56e5b890ba2c
jahdejosefina <jahdejosefina@hotmail.com>
2024-06-17 20:42:07 -0300

    cambios

--- src/App.svelte
+++ src/App.svelte
@@ -36,15 +36,18 @@
     2: "lines_03.png",
   }
 
+  
+  /*
   onMount(() => {
     d3.csv("./data/deportistas.csv", d3.autoType).then(data => {
       deportistas = data
       filteredDeportistas = deportistas
     })
   })
-
+  
   $: {
     // Es un observer que se ejecuta cuando cambia el valor de index
+    
     switch (index) {
       case 0:
         filteredDeportistas = deportistas
@@ -65,6 +68,7 @@
     }
     console.log(filteredDeportistas)
   }
+    */
 </script>
 
 <body>
@@ -161,73 +165,73 @@
       </div>
   </div>
   <!-- FIN DE CARRUSEL-->
-
- <!-- GRAFICOS --> 
-  <div class="Graficos-container">
-    <div class="grafico-uno">
-      <h3 class="grafico-title-1">Datos sobre las plataformas mas utilizadas (2023)</h3>
-      <img src="public/imagenes/grafico1.jpg" alt="grafico 1">
-      <p class="texCostado">En noviembre de 2023, la base de usuarios de Midjourney llegaba a  los 16 millones, con usuarios activos diarios que fluctúan 
-        entre 1,2 y 2,5 millones.  Esta sólida participación refleja el éxito de Midjourney a la hora de  captar y retener la atención 
-        de un público diverso. <br> El mayor segmento de su base de usuarios, que comprende 37,76%, se sitúa  en el grupo de edad de 25 a 34 años. Le sigue el grupo 
-        demográfico de 
-        </p>
-        <p class="texAbajo">18 a 25 años, que constituye el 26,18% de sus usuarios.  Estas estadísticas indican que Midjourney es especialmente popular entre  
-          los adultos más jóvenes y los millennials. <br> Los ingresos previstos de Midjourney para finales de 2023 ascienden a la  impresionante 
-          cifra de $300 millones. Esta cifra es un claro indicador  del éxito financiero de la plataforma y de su eficaz estrategia de  monetización. <br>Las 
-          estadísticas que rodean a Midjourney dibujan una imagen vívida de  una plataforma que no sólo está creciendo rápidamente, sino que también 
-          está influyendo profundamente en el panorama del arte generado por IA.  Desde su amplia base de usuarios y sus impresionantes previsiones de 
-          ingresos hasta la entusiasta participación de la comunidad en  plataformas como Discord y Reddit, Midjourney es un testimonio del  potencial 
-          de la IA en el ámbito creativo.</p>
-    </div>
-
-    <div class="grafico-dos">
-      <h3 class="graficos-title-2">Datos sobre la cantidad de imágenes generadas por dia en las plataformas mas usadas (2023)</h3>
-      <img src="public/imagenes/grafico2.jpg" alt="grafico 2">
-
-    </div>
-  </div>
-  <!-- FIN GRAFICOS -->
-
-  <!-- CASO DEL PAPA -->
-   <div>
-    <h3>Impacto de las Imágenes Generadas por IA</h3>
-    <img src="public/imagenes/imagen-papa.jpg" alt="Papa Francisco">
-    <p>Según la revista Forbes, la imagen se creó utilizando Midjourney, una herramienta de inteligencia artificial que permite generar imágenes falsas 
-      increíblemente realistas. Luego, la imagen se compartió el viernes 24 de marzo de 2023  en Reddit antes de llegar a Twitter durante ese fin de 
-      semana. <br>Según un análisis de The New York Times, han obtenido más visitas, “me gusta” y comentarios que muchas otras fotos generadas con IA
-      (229.000 me gusta y 20,6 millones de visitas). A demás, esto provocó una carrera por retratar al líder de 86 años en situaciones cada vez más 
-      extrañas. <br>La prevalencia de Francisco en las imágenes generadas por IA es resultado de una combinación perfecta de factores, según expertos 
-      religiosos. Tras diez años al frente de la Iglesia católica, se lo reconoce al instante en todo el mundo. Se lo considera un líder más accesible que 
-      su predecesor, el papa Benedicto XVI, de línea más dura. Y cuando se combina con un repentino interés por las nuevas herramientas de IA, Francisco, 
-      que en la vida real suele aparecer en ambientes formales, se convierte en la elección recurrente de los creadores de imaganes con IA para colocarlo en 
-      los escenarios más incongruentes.</p>
-   </div>
-
-   <!-- FIN CASO PAPA -->
-
-  <!-- ¿QUÉ DIJO EL PAPA? -->
-   <div class="comentario">
-    <h4>¿Qué dijo el papa? </h4>
-    <p>El papa Francisco ha alertado de los malos usos de la IA, que puede ser un instrumento de “contaminación cognitiva” y “de alteración de la realidad 
-      a través de narrativas parcial o totalmente falsas que se creen, y se comparten, como si fueran verdaderas”. <br>Así ha citado el ejemplo de las 
-      fake news que hoy se sirve de deepfakes, es decir, de la creación y difusión de imágenes que parecen perfectamente verosímiles pero que son falsas. 
-      “También yo he sido objeto de ello”, revela. </p>
-      <!-- TWEETS APARECIENDO -->
-      <div class="container-tweets">
-        <img src="" alt="">
-        <img src="" alt="">
-        <img src="" alt="">
-        <img src="" alt="">
-        <img src="" alt="">ç
-        <img src="" alt="">
-        <img src="" alt="">
-        <img src="" alt="">
+  
+   <!-- GRAFICOS --> 
+    <div class="Graficos-container">
+      <div class="grafico-uno">
+        <h3 class="grafico-title-1">Datos sobre las plataformas mas utilizadas (2023)</h3>
+        <img src="public/imagenes/grafico1.jpg" alt="grafico 1">
+        <p class="texCostado">En noviembre de 2023, la base de usuarios de Midjourney llegaba a  los 16 millones, con usuarios activos diarios que fluctúan 
+          entre 1,2 y 2,5 millones.  Esta sólida participación refleja el éxito de Midjourney a la hora de  captar y retener la atención 
+          de un público diverso. <br> El mayor segmento de su base de usuarios, que comprende 37,76%, se sitúa  en el grupo de edad de 25 a 34 años. Le sigue el grupo 
+          demográfico de 
+          </p>
+          <p class="texAbajo">18 a 25 años, que constituye el 26,18% de sus usuarios.  Estas estadísticas indican que Midjourney es especialmente popular entre  
+            los adultos más jóvenes y los millennials. <br> Los ingresos previstos de Midjourney para finales de 2023 ascienden a la  impresionante 
+            cifra de $300 millones. Esta cifra es un claro indicador  del éxito financiero de la plataforma y de su eficaz estrategia de  monetización. <br>Las 
+            estadísticas que rodean a Midjourney dibujan una imagen vívida de  una plataforma que no sólo está creciendo rápidamente, sino que también 
+            está influyendo profundamente en el panorama del arte generado por IA.  Desde su amplia base de usuarios y sus impresionantes previsiones de 
+            ingresos hasta la entusiasta participación de la comunidad en  plataformas como Discord y Reddit, Midjourney es un testimonio del  potencial 
+            de la IA en el ámbito creativo.</p>
       </div>
-   </div>
-
-
-  </main>
+  
+      <div class="grafico-dos">
+        <h3 class="graficos-title-2">Datos sobre la cantidad de imágenes generadas por dia en las plataformas mas usadas (2023)</h3>
+        <img src="public/imagenes/grafico2.jpg" alt="grafico 2">
+  
+      </div>
+    </div>
+    <!-- FIN GRAFICOS -->
+  
+    <!-- CASO DEL PAPA -->
+     <div>
+      <h3>Impacto de las Imágenes Generadas por IA</h3>
+      <img src="public/imagenes/imagen-papa.jpg" alt="Papa Francisco">
+      <p>Según la revista Forbes, la imagen se creó utilizando Midjourney, una herramienta de inteligencia artificial que permite generar imágenes falsas 
+        increíblemente realistas. Luego, la imagen se compartió el viernes 24 de marzo de 2023  en Reddit antes de llegar a Twitter durante ese fin de 
+        semana. <br>Según un análisis de The New York Times, han obtenido más visitas, “me gusta” y comentarios que muchas otras fotos generadas con IA
+        (229.000 me gusta y 20,6 millones de visitas). A demás, esto provocó una carrera por retratar al líder de 86 años en situaciones cada vez más 
+        extrañas. <br>La prevalencia de Francisco en las imágenes generadas por IA es resultado de una combinación perfecta de factores, según expertos 
+        religiosos. Tras diez años al frente de la Iglesia católica, se lo reconoce al instante en todo el mundo. Se lo considera un líder más accesible que 
+        su predecesor, el papa Benedicto XVI, de línea más dura. Y cuando se combina con un repentino interés por las nuevas herramientas de IA, Francisco, 
+        que en la vida real suele aparecer en ambientes formales, se convierte en la elección recurrente de los creadores de imaganes con IA para colocarlo en 
+        los escenarios más incongruentes.</p>
+     </div>
+  
+     <!-- FIN CASO PAPA -->
+  
+    <!-- ¿QUÉ DIJO EL PAPA? -->
+     <div class="comentario">
+      <h4>¿Qué dijo el papa? </h4>
+      <p>El papa Francisco ha alertado de los malos usos de la IA, que puede ser un instrumento de “contaminación cognitiva” y “de alteración de la realidad 
+        a través de narrativas parcial o totalmente falsas que se creen, y se comparten, como si fueran verdaderas”. <br>Así ha citado el ejemplo de las 
+        fake news que hoy se sirve de deepfakes, es decir, de la creación y difusión de imágenes que parecen perfectamente verosímiles pero que son falsas. 
+        “También yo he sido objeto de ello”, revela. </p>
+        <!-- TWEETS APARECIENDO -->
+        <div class="container-tweets">
+          <img src="" alt="">
+          <img src="" alt="">
+          <img src="" alt="">
+          <img src="" alt="">
+          <img src="" alt="">ç
+          <img src="" alt="">
+          <img src="" alt="">
+          <img src="" alt="">
+        </div>
+     </div>
+  
+  
+</main>
 </body>
 
 <!-- 
@@ -326,9 +330,6 @@
 
 -->
 
-<div class="lorem_ipsum">
-  <Loremipsum />
-</div>
 
 <style>
  
